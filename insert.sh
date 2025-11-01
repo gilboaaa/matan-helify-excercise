0ca38aa003d91c10b5844a2b0f140a5c7279385f
gilboaaa <your_email@example.com>
2025-11-01 06:31:37 +0200

    test

--- insert.sh
+++ insert.sh
@@ -4,4 +4,5 @@ docker compose exec tidb bash -lc "yum install -y mysql"
 for i in $(seq 1 500); do
     name="matan$i"
     email="matan$i@test.com"
-    docker compose exec tidb mysql -u root -h localhost -P4000 -e "INSERT INTO helify_cdc_demo.users (name, email) VALUES ('$name', '$email');"
+    docker compose exec tidb mysql -htidb -u root -P4000 -e "INSERT INTO helify_cdc_demo.users (name, email) VALUES ('$name', '$email');"
+done
